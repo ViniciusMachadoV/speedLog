3c8a3925849d5a5561d513fe3ea349eb299af5e3
Vinícius <viannaviniciusm@gmail.com>
2023-03-06 08:17:18 -0300

    general syntax and connect function changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,8 +1,5 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
-
 	public function index()
 	{
 		$this->load->model('model_Admin');
@@ -14,4 +11,15 @@ class Admin extends CI_Controller {
 		$this->load->view('pages/view_admin',$dados);
 		$this->load->view('template/footer');
 	}
-}
+	public function registerUser()
+	{
+		// $name_signUp = $_POST['name'];
+		// $email_SignUp = $_POST['email'];
+		// $cpf_signUp = $_POST['cpf'];
+		// $nickname_SignUp = $_POST['nick'];
+		// $phoneNumber_SignUp = $_POST['phone'];
+		// $pass_SignUp = $_POST['pass'];
+		// $this->load->model('model_Connect');
+        // $this->model_Connect->register($name_signUp,$email_SignUp,$cpf_signUp,$nickname_SignUp,$phoneNumber_SignUp,$pass_SignUp);
+	}
+}?>
