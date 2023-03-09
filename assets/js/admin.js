13180cc4b9736c9b207510012fef840d5f740232
Vinícius <viannaviniciusm@gmail.com>
2023-03-09 11:30:58 -0300

    admin + sql update

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,11 +1,7 @@
-$(document).ready(function () {
-	$("#cpfAdmin").mask("000.000.000-00");
-	$("#telefoneAdmin").mask("(00)0 0000-0000");
-    $(".tabAdmin").hide();
-    $(".listDeliveryman").show();
-});
-$(".tabBtn").click(function(){
-    $(".tabAdmin").hide();
+$(document).ready(function(){
+    $(".listDeliveryman").hide();
+    $(".listDeliveries").hide();
+    $(".registerAdmins").hide();
 });
 $(".btnListDeliveryman").click(function(){
     $(".listDeliveryman").show();
@@ -15,7 +11,4 @@ $(".btnListDeliveries").click(function(){
 });
 $(".btnRegisterAdmins").click(function(){
     $(".registerAdmins").show();
-});
-$(".adjustServicesVariables").click(function(){
-    $(".adjustServicesVariables").show();
 });
