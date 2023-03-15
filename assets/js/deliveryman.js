0081acbcbd95163c4bfe77ae052f6fd7b6a38029
Vinícius <viannaviniciusm@gmail.com>
2023-03-15 08:37:11 -0300

    deliveryman tabs

--- assets/js/deliveryman.js
+++ assets/js/deliveryman.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-  // $(".signUpType").hide();
-  // $(".signUp").hide();
+  $(".orderTab").hide();
+  $("#listPendingOrders").show();
+});
+$('#btnPendingOrders').click(function () {
+  $(".orderTab").hide();
+  $("#listPendingOrders").show();
+});
+$('#btnOngoingOrders').click(function () {
+  $(".orderTab").hide();
+  $("#listOngoingOrders").show();
+});
+$('#btnFinishedOrders').click(function () {
+  $(".orderTab").hide();
+  $("#listFinishedOrders").show();
 });
 $('.confirmar').click(function () {
   var idCard = this.id;
       $.post("deliveryman/confirmarPedido", {idPedido: idCard});
       location.reload();
+});
+$('.cancelar').click(function () {
+  $.post("deliveryman/cancelarPedido",{entregaId:this.id});
+});
+$('.concluir').click(function () {
+  $.post("deliveryman/concluirPedido",{entregaId:this.id});
 });
